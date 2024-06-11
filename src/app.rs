cd1015e0e3036fd3039620447ab581b579f64ceb
cxrvus <benito.m.rabe@gmail.com>
2024-06-11 21:01:06 +0200

    UI hello world

---
+++ src/app.rs
@@ -0,0 +1,5 @@
+use eframe::{self, egui::Ui};
+
+pub fn main(ui: &mut Ui) {
+	ui.heading("Hello World");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+mod app;
+use eframe::{egui::{self, Context}, Frame, NativeOptions};
+// use std::{fs, path::PathBuf};
+
 fn main() {
-    println!("Hello, world!");
+	let options = NativeOptions::default();
+	eframe::run_simple_native("CX Followers", options, update).unwrap();
+}
+
+fn update(ctx: &Context, _frame: &mut Frame) {
+	egui::CentralPanel::default().show(ctx, |ui| { app::main(ui)} );
 }
